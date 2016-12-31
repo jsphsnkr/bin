7469fc1f906809e38f30d886fcf60eca1850a535
jsphsnkr <jlsinker@gmail.com>
2016-12-31 10:49:56 +0000

    cleanup

--- i3snaps.sh
+++ i3snaps.sh
@@ -1,40 +1,41 @@
 #!/bin/sh
 
 #
-# snaps.sh - snaps windows to corners, middle, etc.
+# i3snaps.sh - snaps windows to corners, middle, etc.
 #
 
-# source global vars
-. $HOME/bin/wmu/wmu.conf
+PH=16
+GAP=8
+BW=2
 
 CUR=${2:-$(pfw)}
 ROOT=$(lsw -r)
 SW=$(wattr w $ROOT)
 SH=$(wattr h $ROOT)
 
-BW=$(wattr b $CUR)
+#BW=$(wattr b $CUR)
 W=$(wattr w $CUR)
 H=$(wattr h $CUR)
 CX=$(wattr x $CUR)
 CY=$(wattr y $CUR)
 
-X=$GAP
-Y=$((PH + GAP))
+X=$((GAP + BW))
+Y=$((PH + GAP + BW))
 
 case $1 in
     tr) # top-right
-		X=$((SW - W - 2*BW - GAP)) 
+		X=$((SW - W - BW - GAP)) 
 		;;
     bl) # bottom-left
-		Y=$((SH - H  - 2*BW - GAP)) 
+		Y=$((SH - H  - BW - GAP)) 
 		;;
     br) # bottom-right
-		X=$((SW - W - 2*BW - GAP))
-        Y=$((SH - H - 2*BW - GAP)) 
+		X=$((SW - W - BW - GAP))
+        Y=$((SH - H - BW - GAP)) 
 		;;
     md) # middle
 		X=$((SW/2 - W/2 - BW))
-        Y=$((SH/2 - H/2 - BW)) 
+		Y=$((SH/2 - H/2 + PH/2)) 
 		;;
 	mm) # venam mode (half screen width && centered)
 		W=$((SW/2 - 2*BW - GAP))
@@ -44,16 +45,6 @@ case $1 in
     mx) # maximised respecting panel
 		W=$((SW - 2*BW - 2*GAP))
         H=$((SH - PH - 2*BW - 2*GAP))
-		;;
-	vx) # maximise vertically to bottom edge
-		H=$((SH - CY - 2*BW -GAP))
-		X=$CX
-		Y=$CY
-		;;
-	hx) #maximise horizontally to right edge
-		W=$((SW - CX - 2*BW - GAP))
-		X=$CX
-		Y=$CY
 		;;
 esac
 
